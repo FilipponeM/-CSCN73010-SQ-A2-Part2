a27e7baccb0c129098cf6f6a7fbf366bcf8a5101
Matteo Filippone <115741060+FilipponeM@users.noreply.github.com>
2024-10-28 14:28:06 -0400

    testing workflow

--- Image-Classifier-main/test_modelcreation.py
+++ Image-Classifier-main/test_modelcreation.py
@@ -3,7 +3,7 @@ class TestApp:
     def test_one_imageIO_pass(self):
         class_names = ['plane', 'car', 'bird', 'cat', 'deer', 'dog', 'frog', 'horse', 'ship', 'truck']
 
-        class_check = ['x', 'car', 'bird', 'cat', 'deer', 'dog', 'frog', 'horse', 'ship', 'truck']
+        class_check = ['plane', 'car', 'bird', 'cat', 'deer', 'dog', 'frog', 'horse', 'ship', 'truck']
 
         assert class_check == class_names
 
