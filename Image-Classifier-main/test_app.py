58b679a2aea0077750b031883d4015045d8ff8df
Matteo Filippone <115741060+FilipponeM@users.noreply.github.com>
2024-10-28 14:30:46 -0400

    testing workflow

--- Image-Classifier-main/test_app.py
+++ Image-Classifier-main/test_app.py
@@ -16,24 +16,12 @@ class TestApp:
 
         assert class_check == class_names
 
-    def test_two_CIFAR10Names_fail(self):
-        class_names = ['plane', 'car', 'bird', 'cat', 'deer', 'dog', 'frog', 'horse', 'ship', 'truck']
-
-        class_check = ['x', 'y', 'z']
-
-        assert class_check == class_names
-
     def test_three_imageIO_pass(self):
         # Assuming you have a valid image file for testing
         img_path = 'uploads/uploaded_image.png'  # Replace with a valid image path
         img = self.preprocess_image(img_path)
         assert img.shape == (1, 32, 32, 3)  # Use assert to check shape
 
-    def test_four_imageIO_fail(self):
-        # Assuming you have a valid image file for testing
-        img_path = 'uploads/uploaded_image.png'  # Replace with a valid image path
-        img = self.preprocess_image(img_path)
-        assert img.shape == (1, 32, 32, 3)  # Use assert to check shape
 
     #def test_five_pass(self):
 
--- Image-Classifier-main/test_modelcreation.py
+++ Image-Classifier-main/test_modelcreation.py
@@ -6,10 +6,3 @@ class TestApp:
         class_check = ['plane', 'car', 'bird', 'cat', 'deer', 'dog', 'frog', 'horse', 'ship', 'truck']
 
         assert class_check == class_names
-
-    def test_two_imageIO_fail(self):
-        class_names = ['plane', 'car', 'bird', 'cat', 'deer', 'dog', 'frog', 'horse', 'ship', 'truck']
-
-        class_check = ['x', 'y', 'z']
-
-        assert class_check == class_names
