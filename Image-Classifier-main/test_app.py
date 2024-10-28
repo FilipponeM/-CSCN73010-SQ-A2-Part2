25946e2553af957c69b9368eb5acc26e7a00751e
Matteo Filippone <115741060+FilipponeM@users.noreply.github.com>
2024-10-28 12:24:11 -0400

    Adding tests (pytests)

---
+++ Image-Classifier-main/test_app.py
@@ -0,0 +1,44 @@
+import cv2 as cv
+
+class TestApp:
+
+    def preprocess_image(self, img_path):
+        img = cv.imread(img_path)
+        img = cv.resize(img, (32, 32))
+        img = img / 255.0
+        img = img[None, :]  # Add batch dimension
+        return img
+
+    def test_one_pass(self):
+        class_names = ['plane', 'car', 'bird', 'cat', 'deer', 'dog', 'frog', 'horse', 'ship', 'truck']
+
+        class_check = ['plane', 'car', 'bird', 'cat', 'deer', 'dog', 'frog', 'horse', 'ship', 'truck']
+
+        assert class_check == class_names
+
+    def test_two_fail(self):
+        class_names = ['plane', 'car', 'bird', 'cat', 'deer', 'dog', 'frog', 'horse', 'ship', 'truck']
+
+        class_check = ['x', 'y', 'z']
+
+        assert class_check == class_names
+
+    def test_three_pass(self):
+        # Assuming you have a valid image file for testing
+        img_path = 'uploads/uploaded_image.png'  # Replace with a valid image path
+        img = self.preprocess_image(img_path)
+        assert img.shape == (1, 32, 32, 3)  # Use assert to check shape
+
+    def test_four_fail(self):
+        # Assuming you have a valid image file for testing
+        img_path = 'uploads/uploaded_image.png'  # Replace with a valid image path
+        img = self.preprocess_image(img_path)
+        assert img.shape == (1, 32, 32, 3)  # Use assert to check shape
+
+    def test_five_pass(self):
+        app = Flask(__name__)
+
+
+
+
+        
---
+++ Image-Classifier-main/test_result.py
@@ -0,0 +1,15 @@
+class TestApp:
+
+    def test_one_pass(self):
+        class_names = ['plane', 'car', 'bird', 'cat', 'deer', 'dog', 'frog', 'horse', 'ship', 'truck']
+
+        class_check = ['plane', 'car', 'bird', 'cat', 'deer', 'dog', 'frog', 'horse', 'ship', 'truck']
+
+        assert class_check == class_names
+
+    def test_two_fail(self):
+        class_names = ['plane', 'car', 'bird', 'cat', 'deer', 'dog', 'frog', 'horse', 'ship', 'truck']
+
+        class_check = ['x', 'y', 'z']
+
+        assert class_check == class_names
