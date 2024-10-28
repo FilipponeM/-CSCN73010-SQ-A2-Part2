fecbe7ab572a5a32f0357afc89cc5a5dd2bb73f2
Matteo Filippone <115741060+FilipponeM@users.noreply.github.com>
2024-10-28 17:17:41 -0400

    Update lint.py for testing

--- Image-Classifier-main/lint.py
+++ Image-Classifier-main/lint.py
@@ -6,7 +6,7 @@ from pylint import lint
 
 #THRESHOLD = 7 
 
-run = lint.Run(["Image-Classifier-main/modelcreation", "Image-Classifier-main/app.py", "Image-Classifier-main/result.py"], exit=False) 
+run = lint.Run(["Image-Classifier-main/modelcreation", "Image-Classifier-main/app", "Image-Classifier-main/result"], exit=False) 
 
 score = run.linter.stats.global_note 
 
